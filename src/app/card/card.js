53de66a495bdc9a9d857003da01f815e7153aa45
123 <1831454713@qq.com>
2016-10-13 15:30:15 +0800

    错误0.0.1

--- src/app/card/card.js
+++ src/app/card/card.js
@@ -1,4 +1,4 @@
- angular.module('ui.bootstrap.demo').controller('TimepickerDemoCtrl', function ($scope) { 
+ angular.module('coo').controller('TimepickerDemoCtrl', function ($scope) { 
  $scope.mytime = new Date();
 
  
