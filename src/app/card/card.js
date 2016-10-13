0d88ecf36b8409c252927d09cccdd337ce6b65bb
unknown <>
2016-10-13 10:32:22 +0800

    card

---
+++ src/app/card/card.js
@@ -0,0 +1,6 @@
+ angular.module('ui.bootstrap.demo').controller('TimepickerDemoCtrl', function ($scope, $log) { 
+ $scope.mytime = new Date();
+
+ 
+ });
+ 
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('card', {
+        url: '/card',
+        templateUrl: 'app/card/card.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
