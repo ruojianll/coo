43177d92c61d7c314ebea7d2b12aeb7a619b4243
mll <2872802297@qq.com>
2016-10-13 09:49:01 +0800

    denglu

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('login', {
+        url: '/denglu',
+        templateUrl: 'app/login/denglu.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
