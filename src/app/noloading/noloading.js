832ac5aa25890986ec68098b4ab7fdc2fdb4bbfd
123 <1831454713@qq.com>
2016-10-17 15:18:40 +0800

    nologin

--- src/app/noloading/noloading.js
+++ src/app/noloading/noloading.js
@@ -1,10 +1,9 @@
 angular.module("coo").controller("noloading",function($scope){
-	alert(localStorage.getItem("name"))
-	if(localStorage.getItem("name")!=undefined){
-		$scope.nologin="您以登录 。";
-		$scope.nologinNo=""
-	}else{
+	 if(localStorage.getItem("name")==null||localStorage.getItem("name")==""){
 		$scope.nologin="您暂未登录 ，";
 		$scope.nologinNo="请登录"
+	}else{
+		$scope.nologin="您以登录 。";
+		$scope.nologinNo=""
 	}
 })
