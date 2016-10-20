f90cd1a758c21b1f7716e72cb488dcb39d295236
xxb123456 <1518643526@qq.com>
2016-10-20 09:44:31 +0800

    dsd

--- src/app/lyan/liuyan.js
+++ src/app/lyan/liuyan.js
@@ -36,25 +36,25 @@ angular.module("coo").controller("yan",function($scope,$http,apiServ,environment
         return(clock); 
 }
 				var sp3=document.createElement('span')
-				sp3.id='sp1'
-				sp3.innerHTML=te.value
+				sp3.id='yan_sp1'
+				sp3.innerHTML=yan_te.value
 				var sp4=document.createElement('span')
-				sp4.id='sp2'
+				sp4.id='yan_sp2'
 				var op1=document.createElement('p')
-				op1.id='op'
+				op1.id='yan_op'
 				var sp5=document.createElement('a')
-				sp5.id='right'
+				sp5.id='yan_right'
 				sp5.innerHTML=CurentTime()
 				var sp6=document.createElement('a')
-				sp6.className='name'
+				sp6.className='yan_name'
 				sp6.innerHTML=$scope.user_name
-				if(te.value==''){
+				if(yan_te.value==''){
 				}else{
 				sp4.appendChild(sp5)
 				sp4.appendChild(sp6)
 				op1.appendChild(sp3)
 				op1.appendChild(sp4)
-				div1.appendChild(op1)
-				te.value=''}
+				yan_div1.appendChild(op1)
+				yan_te.value=''}
 	}
 })
